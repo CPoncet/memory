a2a34d4f7082940b239dc273cf285dfbc46c6756
Clément Poncet <contact.clementponcet@gmail.com>
2022-10-08 13:28:39 +0200

    simple timer and win/lose states

--- client/src/pages/game.js
+++ client/src/pages/game.js
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from '../components/card';
 import useCards, { cardsName } from '../hooks/useCards';
 
 const Game = () => {
   const cards = useCards();
+
+  const initialTimer = 60;
+
+  // Init timer in seconds
+  const [timeLeft, setTimeLeft] = useState(initialTimer);
+  const [won, setWon] = useState(false);
+
   const [clickCount, setClickCount] = useState(0);
   const [cardsList, setCardsList] = useState(cardsName);
   //const [firstFruit, setFirstFruit] = useState({});
@@ -19,10 +26,8 @@ const Game = () => {
     switch (clickCount) {
       case 0:
         setClickCount(1);
-        //setFirstFruit(detail);
         const joinedFirstFruit = [...selectedFruits, detail];
         setSelectedFruits(joinedFirstFruit);
-        console.log(selectedFruits);
         break;
       case 1:
         setClickCount(2);
@@ -37,39 +42,60 @@ const Game = () => {
             (card) => card !== joinedFruits[0] && card !== joinedFruits[1]
           );
           setCardsList(newCardsList);
+
+          if (!newCardsList.length) {
+            setWon(true);
+          }
         }
-        setTimeout(() => {
-          setClickCount(0);
-          // setFirstFruit('');
-          setSelectedFruits([]);
-        }, 1000);
+        // Timeout pour figer les cartes pendant 1s, puis les retourner
+        setClickCount(0);
+        setSelectedFruits([]);
         break;
       default:
         break;
     }
   };
-  console.log('outside', selectedFruits);
-  console.log(cardsList.length);
+
+  useEffect(() => {
+    if (won) {
+      alert('gagné en ' + (initialTimer - timeLeft) + 's');
+    }
+
+    if (timeLeft > 0 && !won) {
+      let interval;
+      interval = setInterval(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearInterval(interval);
+    }
+  }, [timeLeft, won]);
 
   return (
     <>
-      <h1>Il reste x paires à trouver</h1>
+      <h1>Il reste {cardsList.length / 2} paires à trouver</h1>
       <div className="cards-grid">
-        {cards.map((card, index) => (
-          <div
-            key={`card-${index}`}
-            onClick={() => handleCardClick(card.detail)}
-          >
-            <Card
-              coords={card.coords}
-              show={
-                selectedFruits.find((el) => el === card.detail) ? true : false
-              }
-              found={cardsList.find((el) => el === card.detail) ? false : true}
-            />
-          </div>
-        ))}
+        {!won
+          ? timeLeft > 0
+            ? cards.map((card, index) => (
+                <div
+                  key={`card-${index}`}
+                  onClick={() => handleCardClick(card.detail)}
+                >
+                  <Card
+                    coords={card.coords}
+                    show={
+                      selectedFruits.find((el) => el === card.detail)
+                        ? true
+                        : false
+                    }
+                    found={
+                      cardsList.find((el) => el === card.detail) ? false : true
+                    }
+                  />
+                </div>
+              ))
+            : 'Perdu'
+          : 'Gagné'}
       </div>
+      <h1>Il reste {timeLeft} secondes</h1>
     </>
   );
 };
